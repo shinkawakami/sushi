6119cf6d959e5712788b38fb22d73672b6335bef
shinkawakami <snnsk.2002@gmail.com>
2023-07-08 16:28:51 +0900

    Add model

--- database/migrations/2023_07_08_154541_create_comments_table.php
+++ database/migrations/2023_07_08_154541_create_comments_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('post_id')->constrained();
+            $table->string('body');
             $table->timestamps();
         });
     }

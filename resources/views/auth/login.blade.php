adb4f63e33c096f81fecf1efc7d3b4a2feba0069
nanazawa06 <nanazawa06@gmail.com>
2023-07-09 14:54:01 +0900

    ログイン画面とログイン後のページの変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,4 +44,9 @@
             </x-primary-button>
         </div>
     </form>
+    <div class="to-register">
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('新規登録') }}
+                </a>
+    </div>
 </x-guest-layout>
